78cf10d275ec38a13a2af03050dc2cc9421b6d3d
Alex Ward <alexanderward5@gmail.com>
2024-07-04 22:04:50 -0600

    initial setup and poc

---
+++ index.py
@@ -0,0 +1,37 @@
+import requests
+from pprint import pprint
+from bs4 import BeautifulSoup
+
+resp_html = requests.get("https://fbref.com/en/comps/9/Premier-League-Stats").text
+
+soup = BeautifulSoup(resp_html, "html.parser")
+
+table_headers = []
+table_rows = []
+
+for row_num, tr in enumerate(soup.find("table").find_all("tr")):
+    # Parse table headers into array of dicts
+    if row_num == 0:
+        for th in tr.find_all("th"):
+            table_headers.append(
+                {
+                    "text": th.text,
+                    "data-stat": th["data-stat"],
+                    "aria-label": th["aria-label"],
+                }
+            )
+    # Parse table data into array of dicts
+    else:
+        tr_dict = {}
+        for td in tr.find_all("td"):
+            data_stat = td["data-stat"]
+            tr_dict[data_stat] = td.text
+
+            data_link = td.find("a")
+            if data_link:
+                tr_dict[f"{data_stat}_url"] = f'https://fbref.com{data_link["href"]}'
+
+        if tr_dict:
+            table_rows.append(tr_dict)
+
+pprint(table_rows[0])
