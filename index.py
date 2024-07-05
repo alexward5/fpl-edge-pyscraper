49961c46aaf2e9614e620a607f4c06ee8969447d
Alex Ward <alexanderward5@gmail.com>
2024-07-04 22:34:18 -0600

    convert logic to class

--- index.py
+++ index.py
@@ -2,36 +2,52 @@ import requests
 from pprint import pprint
 from bs4 import BeautifulSoup
 
-resp_html = requests.get("https://fbref.com/en/comps/9/Premier-League-Stats").text
 
-soup = BeautifulSoup(resp_html, "html.parser")
+class FBRef_Table:
+    def __init__(self, table_url, table_index):
+        self.table_url = table_url
+        self.table_html = requests.get(self.table_url).text
+        self.table_headers = []
+        self.table_rows = []
 
-table_headers = []
-table_rows = []
+        self._parse_table()
 
-for row_num, tr in enumerate(soup.find("table").find_all("tr")):
-    # Parse table headers into array of dicts
-    if row_num == 0:
-        for th in tr.find_all("th"):
-            table_headers.append(
-                {
-                    "text": th.text,
-                    "data-stat": th["data-stat"],
-                    "aria-label": th["aria-label"],
-                }
-            )
-    # Parse table data into array of dicts
-    else:
-        tr_dict = {}
-        for td in tr.find_all("td"):
-            tr_dict[data_stat] = td.text
-            data_stat = td["data-stat"]
+    def get_headers(self):
+        return self.table_headers
 
-            data_link = td.find("a")
-            if data_link:
-                tr_dict[f"{data_stat}_url"] = f'https://fbref.com{data_link["href"]}'
+    def get_rows(self):
+        return self.table_rows
 
-        if tr_dict:
-            table_rows.append(tr_dict)
+    def _parse_table(self):
+        soup = BeautifulSoup(self.table_html, "html.parser")
 
-pprint(table_rows[0])
+        for row_num, tr in enumerate(soup.find("table").find_all("tr")):
+            # Parse table headers into array of dicts
+            if row_num == 0:
+                for th in tr.find_all("th"):
+                    self.table_headers.append(
+                        {
+                            "data-stat": th["data-stat"],
+                            "aria-label": th["aria-label"],
+                            "text": th.text,
+                        }
+                    )
+            # Parse table data into array of dicts
+            else:
+                tr_dict = {}
+                for td in tr.find_all("td"):
+                    data_stat = td["data-stat"]
+                    tr_dict[data_stat] = td.text
+
+                    data_link = td.find("a")
+                    if data_link:
+                        tr_dict[f"{data_stat}_url"] = (
+                            f'https://fbref.com{data_link["href"]}'
+                        )
+
+                if tr_dict:
+                    self.table_rows.append(tr_dict)
+
+
+my_table = FBRef_Table("https://fbref.com/en/comps/9/Premier-League-Stats", 0)
+pprint(my_table.get_rows())
