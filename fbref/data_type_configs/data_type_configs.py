af9bb4a49aac0d49d5ee9f20ef52978eae25625a
Alex Ward <alexanderward5@gmail.com>
2024-07-05 18:24:13 -0600

    rename file

--- fbref/data_type_configs/data_type_configs.py
+++ fbref/data_type_configs/data_type_configs.py
@@ -1,9 +1,7 @@
 from typing import Any
 
-from fbref.data_type_configs.table_configs.fbref_team_overall import (
+from fbref.data_type_configs.table_configs.team_overall_data_types import (
     team_overall_data_types,
 )
 
-data_type_configs: dict[str, Any] = {
-    "fbref_team_overall_data_types": team_overall_data_types
-}
+data_type_configs: dict[str, Any] = {"team_overall_data_types": team_overall_data_types}
