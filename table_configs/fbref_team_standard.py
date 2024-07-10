c92e0326d6f86764d74e879b82470b97d41d8d2a
Alex Ward <alexanderward5@gmail.com>
2024-07-09 22:49:24 -0600

    create config for fbref_team_standard table

---
+++ table_configs/fbref_team_standard.py
@@ -0,0 +1,175 @@
+fbref_team_standard = {
+    "table_name": "fbref_team_standard",
+    "table_column_configs": [
+        {
+            "column_name": "player",
+            "column_type": "VARCHAR (255)",
+            "primary_key": True,
+        },
+        {
+            "column_name": "nationality",
+            "column_type": "VARCHAR (255)",
+            "not_null": True,
+        },
+        {
+            "column_name": "position",
+            "column_type": "VARCHAR (255)",
+            "not_null": True,
+        },
+        {
+            "column_name": "age",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "games",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "games_starts",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "minutes",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "minutes_90s",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "goals",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "assists",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "goals_assists",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "goals_pens",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "pens_made",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "pens_att",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "cards_yellow",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "cards_red",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "xg",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "npxg",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "xg_assist",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "npxg_xg_assist",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "progressive_carries",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "progressive_passes",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "progressive_passes_received",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "goals_per90",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "assists_per90",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "goals_assists_per90",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "goals_pens_per90",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "goals_assists_pens_per90",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "xg_per90",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "xg_assist_per90",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "xg_xg_assist_per90",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "npxg_per90",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "npxg_xg_assist_per90",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "matches",
+            "column_type": "VARCHAR (255)",
+            "not_null": True,
+        },
+    ],
+}
--- table_configs_merged.py
+++ table_configs_merged.py
@@ -1,5 +1,9 @@
 from typing import Any
 
 from table_configs.fbref_team_overall import fbref_team_overall
+from table_configs.fbref_team_standard import fbref_team_standard
 
-merged_table_configs: dict[str, Any] = {"fbref_team_overall": fbref_team_overall}
+merged_table_configs: dict[str, Any] = {
+    "fbref_team_overall": fbref_team_overall,
+    "fbref_team_standard": fbref_team_standard,
+}
