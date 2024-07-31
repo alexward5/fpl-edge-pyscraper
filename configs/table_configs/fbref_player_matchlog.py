78a1f0b293bebff05c566f535a5568d99ccb42b2
Alex Ward <alexanderward5@gmail.com>
2024-07-31 17:44:04 -0600

    create database table config for player matchlog

--- configs/table_configs/fbref_player_matchlog.py
+++ configs/table_configs/fbref_player_matchlog.py
@@ -1,5 +1,191 @@
 fbref_player_matchlog = {
     "table_name": "fbref_team_overall",
     "header_row_index": 1,
-    "table_column_configs": [{}],
+    "table_column_configs": [
+        {
+            "column_name": "date",
+            "column_type": "VARCHAR (255)",
+            "not_null": True,
+        },
+        {
+            "column_name": "dayofweek",
+            "column_type": "VARCHAR (255)",
+            "not_null": True,
+        },
+        {
+            "column_name": "comp",
+            "column_type": "VARCHAR (255)",
+            "not_null": True,
+        },
+        {
+            "column_name": "round",
+            "column_type": "VARCHAR (255)",
+            "not_null": True,
+        },
+        {
+            "column_name": "venue",
+            "column_type": "VARCHAR (255)",
+            "not_null": True,
+        },
+        {
+            "column_name": "result",
+            "column_type": "VARCHAR (255)",
+            "not_null": True,
+        },
+        {
+            "column_name": "team",
+            "column_type": "VARCHAR (255)",
+            "not_null": True,
+        },
+        {
+            "column_name": "opponent",
+            "column_type": "VARCHAR (255)",
+            "not_null": True,
+        },
+        {
+            "column_name": "game_started",
+            "column_type": "VARCHAR (255)",
+            "not_null": True,
+        },
+        {
+            "column_name": "position",
+            "column_type": "VARCHAR (255)",
+            "not_null": True,
+        },
+        {
+            "column_name": "minutes",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "goals",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "assists",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "pens_made",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "pens_att",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "shots",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "shots_on_target",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "cards_yellow",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "cards_red",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "touches",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "tackles",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "interceptions",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "blocks",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "xg",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "npxg",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "xg_assist",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "sca",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "gca",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "passes_completed",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "passes",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "passes_pct",
+            "column_type": "DECIMAL",
+            "not_null": True,
+        },
+        {
+            "column_name": "progressive_passes",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "carries",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "progressive_carries",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "take_ons",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "take_ons_won",
+            "column_type": "INT",
+            "not_null": True,
+        },
+        {
+            "column_name": "match_report",
+            "column_type": "VARCHAR (255)",
+            "not_null": True,
+        },
+    ],
 }
