00079b8089db23928ec3fcd1908fc12f9c3a98af
Alex Ward <alexanderward5@gmail.com>
2024-08-17 11:16:43 -0600

    add functionality to pass data from parent table to child

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from time import sleep
+from typing import Optional
 
 from configs.run_config import fbref_table_config
 from configs.table_configs_merged import merged_table_configs
@@ -8,19 +8,17 @@ from utils.seed_table import seed_table
 schema_name = "test_schema"
 
 
-def recursive_run(fbref_table_config: dict, table_url: str = ""):
+def recursive_run(
+    fbref_table_config: dict, table_url: str = "", parent_field: Optional[dict] = None
+):
     table_name = fbref_table_config["table_name"]
     table_config = merged_table_configs[table_name]
 
     table_url = fbref_table_config.get("table_url") or table_url
 
     fbref_table = FBRef_Table(
-        table_url=table_url,
-        table_config=fbref_table_config,
+        table_url=table_url, table_config=fbref_table_config, custom_column=parent_field
     )
-
-    # Sleep for 10s between runs to avoid reaching rate limit
-    sleep(10)
 
     seed_table(
         schema_name=schema_name,
@@ -30,23 +28,48 @@ def recursive_run(fbref_table_config: dict, table_url: str = ""):
         table_rows=fbref_table.table_rows,
     )
 
-    sub_table_urls = []
+    sub_tables_metadata: list[dict] = []
     if fbref_table_config.get("sub_table_config"):
         sub_table_config = fbref_table_config["sub_table_config"]
         hyperlink_data_stat = sub_table_config["hyperlink_data_stat"]
 
         for row_data in fbref_table.table_rows:
+            sub_table_metadata = {}
+
             hyperlink_cell = next(
                 item for item in row_data if item["data_stat"] == hyperlink_data_stat
             )
 
-            sub_table_urls.append(hyperlink_cell["data_hyperlink"])
+            if hyperlink_cell.get("data_hyperlink"):
+                sub_table_metadata["data_hyperlink"] = hyperlink_cell["data_hyperlink"]
 
-    if sub_table_urls:
-        for sub_table_url in sub_table_urls:
-            recursive_run(
-                fbref_table_config["sub_table_config"], table_url=sub_table_url
-            )
+            if sub_table_config.get("include_parent_field"):
+                parent_field_data_stat = sub_table_config["include_parent_field"]
+                parent_field_cell = next(
+                    item
+                    for item in row_data
+                    if item["data_stat"] == parent_field_data_stat
+                )
+
+                sub_table_metadata["parent_field"] = {
+                    "data_stat": parent_field_cell["data_stat"],
+                    "data_value": parent_field_cell["data_value"],
+                }
+
+            if sub_table_metadata.get("data_hyperlink"):
+                sub_tables_metadata.append(sub_table_metadata)
+
+        if sub_tables_metadata:
+            for sub_table_metadata in sub_tables_metadata:
+                run_args = {
+                    "fbref_table_config": fbref_table_config["sub_table_config"],
+                    "table_url": sub_table_metadata["data_hyperlink"],
+                }
+
+                if sub_table_config.get("include_parent_field"):
+                    run_args["parent_field"] = sub_table_metadata["parent_field"]
+
+                recursive_run(**run_args)
 
 
 recursive_run(fbref_table_config=fbref_table_config)
