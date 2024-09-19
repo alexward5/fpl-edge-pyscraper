3dc072b7a23f91df2ce7cb36a4cdbdc9f6eca24a
Alex Ward <alexanderward5@gmail.com>
2024-09-19 17:28:41 -0600

    conditionally recreate schema and tables

--- index_new.py
+++ index_new.py
@@ -1,3 +1,12 @@
+from configs.run_config import fbref_table_config
 from utils.create_schema_and_tables import create_schema_and_tables
+from utils.process_fbref_table import process_fbref_table
 
-create_schema_and_tables()
+CREATE_SCHEMA_AND_TABLES = False
+
+if CREATE_SCHEMA_AND_TABLES:
+    create_schema_and_tables()
+
+process_fbref_table(
+    table_url=fbref_table_config["table_url"], fbref_table_config=fbref_table_config
+)
