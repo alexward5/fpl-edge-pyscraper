1cfa1d1b258fc00ac3867e2a9089bbe3a955de4f
Alex Ward <alexanderward5@gmail.com>
2024-09-30 17:24:51 -0600

    create helper function for getting list of child table urls from parent table

---
+++ utils/get_child_table_urls.py
@@ -0,0 +1,17 @@
+from fbref.FBRef_Table import FBRef_Table
+
+
+def get_child_table_urls(
+    fbref_table: FBRef_Table, hyperlink_data_stat: str
+) -> list[str]:
+    child_table_urls = []
+
+    for row_data in fbref_table.table_rows:
+        child_table_url = next(
+            item for item in row_data if item["data_stat"] == hyperlink_data_stat
+        )["data_hyperlink"]
+
+        if child_table_url:
+            child_table_urls.append(child_table_url)
+
+    return child_table_urls
