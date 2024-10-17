e357a19d3ecf2580b5e3f70b668b2981595376d9
Alex Ward <alexanderward5@gmail.com>
2024-10-17 14:05:47 -0600

    boilerplate to run fpl api script

--- index.py
+++ index.py
@@ -1,11 +1,13 @@
 from configs.run_config import fbref_table_config
 from run_scripts.create_schema_and_tables import create_schema_and_tables
 from run_scripts.process_fbref_table import process_fbref_table
+from run_scripts.process_fpl_api_data import process_fpl_api_data
 
 SCHEMA_NAME = "test_schema_new"
 
 CREATE_SCHEMA_AND_TABLES = True
 PROCESS_FBREF_TABLES = True
+PROCESS_FPL_API_DATA = True
 
 if CREATE_SCHEMA_AND_TABLES:
     create_schema_and_tables(schema_name=SCHEMA_NAME)
@@ -16,3 +18,6 @@ if PROCESS_FBREF_TABLES:
         table_url=fbref_table_config["table_url"],
         fbref_table_config=fbref_table_config,
     )
+
+if PROCESS_FPL_API_DATA:
+    process_fpl_api_data()
---
+++ run_scripts/process_fpl_api_data.py
@@ -0,0 +1,2 @@
+def process_fpl_api_data() -> None:
+    pass
