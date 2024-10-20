15267bd3de9c74cc44c4e8a05b23b0f4d23b7b68
Alex Ward <alexanderward5@gmail.com>
2024-10-20 14:10:22 -0600

    initial poc for inserting rows of fpl api data

--- run_scripts/process_fpl_api_data.py
+++ run_scripts/process_fpl_api_data.py
@@ -1,5 +1,26 @@
 from fpl_api.FPL_API import FPL_API
+from utils.set_df_dtypes import set_df_dtypes
+from utils.clean_cell_data import clean_cell_data
+from pg.PG import PG
+
+import pandas as pd
+
+pg = PG(dbname="postgres", user="postgres")
 
 
 def process_fpl_api_data() -> None:
-    FPL_API()
+    fpl_api_data = FPL_API()
+
+    fpl_players_df = pd.DataFrame.from_records(fpl_api_data.player_data)
+    fpl_players_df = set_df_dtypes(fpl_players_df)
+
+    # Insert dataframe rows into postgres table
+    for _, row in fpl_players_df.iterrows():
+        cleaned_row_values = [clean_cell_data(val) for val in row.to_list()]
+
+        pg.insert_row(
+            schema="test_schema_new",
+            table_name="fpl_player_data",
+            column_names=fpl_players_df.columns.to_list(),
+            row_values=cleaned_row_values,
+        )
