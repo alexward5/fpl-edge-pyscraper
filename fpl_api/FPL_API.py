f744afac3954b194dc7a1057e3396705d4097709
Alex Ward <alexanderward5@gmail.com>
2024-10-20 13:35:06 -0600

    add method for parsing player/element data

--- fpl_api/FPL_API.py
+++ fpl_api/FPL_API.py
@@ -8,8 +8,9 @@ import requests
 class FPL_API:
     def __init__(
         self,
-    ):
-        self.team_code_mapping = {}
+    ) -> None:
+        self.team_code_mapping: dict = {}
+        self.player_data: list = []
 
         self._api_response_raw = {}
 
@@ -26,9 +27,10 @@ class FPL_API:
         # Check that fpl season data exists before proceeding
         if self._api_response_raw.get("events"):
             self._parse_team_code_mapping()
-            print(self.team_code_mapping)
+            self._parse_player_data()
 
     def _parse_team_code_mapping(self) -> None:
+        # Map fpl team codes to fbref team names
         team_name_mappings = []
         with open("fpl_api/mappings/team_names.csv", "r") as csvfile:
             dict_reader = csv.DictReader(csvfile)
@@ -51,3 +53,12 @@ class FPL_API:
                 fbref_team_name = team_name_mapping["fbref_team_name"]
 
             self.team_code_mapping[team_code] = fbref_team_name
+
+    def _parse_player_data(self) -> None:
+        players = self._api_response_raw["elements"]
+
+        # Add mapped fbref team name to player data
+        for player in players:
+            player["fbref_team_name"] = self.team_code_mapping[player["team_code"]]
+
+        self.player_data = players
