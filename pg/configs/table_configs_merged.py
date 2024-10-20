e48b23468183d28f7754effce5a8ab014c2a047b
Alex Ward <alexanderward5@gmail.com>
2024-10-20 14:09:54 -0600

    create fpl player data config

--- pg/configs/table_configs_merged.py
+++ pg/configs/table_configs_merged.py
@@ -5,9 +5,11 @@ from pg.configs.table_configs.fbref_team_overall import fbref_team_overall
 from pg.configs.table_configs.fbref_team_players_standard import (
     fbref_team_players_standard,
 )
+from pg.configs.table_configs.fpl_player_data import fpl_player_data
 
 table_configs_merged: dict[str, Any] = {
     "fbref_team_overall": fbref_team_overall,
     "fbref_team_players_standard": fbref_team_players_standard,
     "fbref_player_matchlog": fbref_player_matchlog,
+    "fpl_player_data": fpl_player_data,
 }
