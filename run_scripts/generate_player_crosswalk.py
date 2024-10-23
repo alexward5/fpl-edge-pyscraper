edb3e5e2a197265a773b9690c62c5ad8fa48c13d
Alex Ward <alexanderward5@gmail.com>
2024-10-22 18:34:37 -0600

    combine first/last name for fpl players to create full name

--- run_scripts/generate_player_crosswalk.py
+++ run_scripts/generate_player_crosswalk.py
@@ -6,20 +6,28 @@ SCHEMA_NAME = "test_schema_new"
 
 
 def generate_player_crosswalk() -> None:
+    # Create dict of fpl players where each key is a team name and each value is a list of player dicts
+    fpl_players_by_team: dict[str, list[dict]] = {}
+
     fpl_players = pg.query_table(
         schema=SCHEMA_NAME,
         table_name="fpl_player_data",
         columns=["first_name", "second_name", "fbref_team_name", "fpl_row_id"],
     )
 
-    fpl_players_by_team: dict[str, list[dict]] = {}
     for fbref_player in fpl_players:
         player_dict = dict(fbref_player)
+        player_dict["full_name"] = (
+            f"{player_dict['first_name']} {player_dict['second_name']}"
+        )
 
         if fpl_players_by_team.get(fbref_player["fbref_team_name"]):
             fpl_players_by_team[fbref_player["fbref_team_name"]].append(player_dict)
         else:
             fpl_players_by_team[fbref_player["fbref_team_name"]] = [player_dict]
+
+    # Create dict of fbref players where each key is a team name and each value is a list of player dicts
+    fbref_players_by_team: dict[str, list[dict]] = {}
 
     fbref_players = pg.query_table(
         schema=SCHEMA_NAME,
@@ -27,7 +35,6 @@ def generate_player_crosswalk() -> None:
         columns=["player", "team", "fbref_row_id"],
     )
 
-    fbref_players_by_team: dict[str, list[dict]] = {}
     for fbref_player in fbref_players:
         player_dict = dict(fbref_player)
 
