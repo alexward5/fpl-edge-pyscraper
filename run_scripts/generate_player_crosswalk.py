190ce3e36710caa140ffbd06b81491ef65d428bd
Alex Ward <alexanderward5@gmail.com>
2024-10-22 20:47:43 -0600

    add logic to use custom player name mapping when fuzzy match fails

--- run_scripts/generate_player_crosswalk.py
+++ run_scripts/generate_player_crosswalk.py
@@ -1,8 +1,10 @@
+import csv
 import pandas as pd
 
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 from pg.PG import PG
+from utils.find_dict_in_list import find_dict_in_list
 
 pg = PG(dbname="postgres", user="postgres")
 
@@ -17,6 +19,8 @@ def generate_player_crosswalk() -> None:
         schema=SCHEMA_NAME,
         table_name="fpl_player_data",
         columns=["first_name", "second_name", "fbref_team_name", "fpl_row_id"],
+        # Filter out players who have played zero minutes and players who have been removed from game
+        where_clause="WHERE minutes > 0 and status != 'u'",
     )
 
     for fpl_player in fpl_players:
@@ -47,11 +51,14 @@ def generate_player_crosswalk() -> None:
         else:
             fbref_players_by_team[fbref_player["team"]] = [player_dict]
 
+    # Get custom player name mappings for players who don't match automatically
+    player_name_mappings = []
+    with open("fpl_api/mappings/player_names.csv", "r") as csvfile:
+        dict_reader = csv.DictReader(csvfile)
+        player_name_mappings = [row for row in dict_reader]
+
     # Generate crosswalk
     for team_name in fpl_players_by_team:
-        if team_name != "Aston Villa":
-            continue
-
         fpl_player_name_list = [
             player["full_name"]
             for player in fpl_players_by_team[team_name]
@@ -65,13 +72,31 @@ def generate_player_crosswalk() -> None:
 
         # Perform fuzzy matching
         matches = []
-        for item in fpl_player_name_list:
+        for fpl_player_name in fpl_player_name_list:
             match = process.extractOne(
-                item, fbref_player_name_list, scorer=fuzz.token_set_ratio
+                fpl_player_name, fbref_player_name_list, scorer=fuzz.token_set_ratio
             )
             if match:
                 match_score = match[1]
-                matches.append([item, match[0], match_score])
+                if match_score < 70:
+                    player_name_mapping = find_dict_in_list(
+                        list_of_dicts=player_name_mappings,
+                        key="fpl_player_name",
+                        value=fpl_player_name,
+                    )
+                    if not player_name_mapping:
+                        raise ValueError(
+                            f"Custom player name mapping not found for: {fpl_player_name}"
+                        )
+                    matches.append(
+                        [
+                            fpl_player_name,
+                            player_name_mapping["fbref_player_name"],
+                            100,
+                        ]
+                    )
+                else:
+                    matches.append([fpl_player_name, match[0], match_score])
 
         # Convert the output to pandas df
         df = pd.DataFrame(
@@ -79,6 +104,4 @@ def generate_player_crosswalk() -> None:
         )
 
         df_sorted = df.sort_values(by=["score"])
-
-        # Print the DataFrame
         print(df_sorted)
