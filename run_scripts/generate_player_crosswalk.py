055c1447d284d244751862331c91f37b02ebbc82
Alex Ward <alexanderward5@gmail.com>
2024-10-23 17:17:47 -0600

    create df will both fpl and fbref ids and names

--- run_scripts/generate_player_crosswalk.py
+++ run_scripts/generate_player_crosswalk.py
@@ -13,9 +13,9 @@ SCHEMA_NAME = "test_schema_new"
 
 def generate_player_crosswalk() -> None:
     # Create dict of fpl players where each key is a team name and each value is a list of player dicts
-    fpl_players_by_team: dict[str, list[dict]] = {}
+    fpl_player_data_by_team: dict[str, list[dict]] = {}
 
-    fpl_players = pg.query_table(
+    fpl_player_data = pg.query_table(
         schema=SCHEMA_NAME,
         table_name="fpl_player_data",
         columns=["first_name", "second_name", "fbref_team_name", "fpl_row_id"],
@@ -23,33 +23,33 @@ def generate_player_crosswalk() -> None:
         where_clause="WHERE minutes > 0 and status != 'u'",
     )
 
-    for fpl_player in fpl_players:
+    for fpl_player in fpl_player_data:
         player_dict = dict(fpl_player)
         player_dict["full_name"] = (
             f"{player_dict['first_name']} {player_dict['second_name']}"
         )
 
-        if fpl_players_by_team.get(fpl_player["fbref_team_name"]):
-            fpl_players_by_team[fpl_player["fbref_team_name"]].append(player_dict)
+        if fpl_player_data_by_team.get(fpl_player["fbref_team_name"]):
+            fpl_player_data_by_team[fpl_player["fbref_team_name"]].append(player_dict)
         else:
-            fpl_players_by_team[fpl_player["fbref_team_name"]] = [player_dict]
+            fpl_player_data_by_team[fpl_player["fbref_team_name"]] = [player_dict]
 
     # Create dict of fbref players where each key is a team name and each value is a list of player dicts
-    fbref_players_by_team: dict[str, list[dict]] = {}
+    fbref_player_data_by_team: dict[str, list[dict]] = {}
 
-    fbref_players = pg.query_table(
+    fbref_player_data = pg.query_table(
         schema=SCHEMA_NAME,
         table_name="fbref_team_players_standard",
         columns=["player as name", "team", "fbref_row_id"],
     )
 
-    for fbref_player in fbref_players:
+    for fbref_player in fbref_player_data:
         player_dict = dict(fbref_player)
 
-        if fbref_players_by_team.get(fbref_player["team"]):
-            fbref_players_by_team[fbref_player["team"]].append(player_dict)
+        if fbref_player_data_by_team.get(fbref_player["team"]):
+            fbref_player_data_by_team[fbref_player["team"]].append(player_dict)
         else:
-            fbref_players_by_team[fbref_player["team"]] = [player_dict]
+            fbref_player_data_by_team[fbref_player["team"]] = [player_dict]
 
     # Get custom player name mappings for players who don't match automatically
     player_name_mappings = []
@@ -58,24 +58,20 @@ def generate_player_crosswalk() -> None:
         player_name_mappings = [row for row in dict_reader]
 
     # Generate crosswalk
-    for team_name in fpl_players_by_team:
-        fpl_player_name_list = [
-            player["full_name"]
-            for player in fpl_players_by_team[team_name]
-            if player["fbref_team_name"] == team_name
-        ]
-        fbref_player_name_list = [
-            player["name"]
-            for player in fbref_players_by_team[team_name]
-            if player["team"] == team_name
-        ]
+    for team_name in fpl_player_data_by_team:
+        fpl_player_data = fpl_player_data_by_team[team_name]
+        fbref_player_data = fbref_player_data_by_team[team_name]
 
-        # Perform fuzzy matching
-        matches = []
-        for fpl_player_name in fpl_player_name_list:
+        fbref_player_name_list = [player["name"] for player in fbref_player_data]
+
+        # Match fpl player name with fbref player name
+        player_id_crosswalk = []
+        for fpl_player_dict in fpl_player_data:
+            fpl_player_name = fpl_player_dict["full_name"]
             match = process.extractOne(
                 fpl_player_name, fbref_player_name_list, scorer=fuzz.token_set_ratio
             )
+
             if match:
                 match_score = match[1]
                 # If the match score is less than 70, use custom player name mapping
@@ -90,20 +86,49 @@ def generate_player_crosswalk() -> None:
                             f"Custom player name mapping not found for: {fpl_player_name}"
                         )
 
-                    matches.append(
-                        [
-                            fpl_player_name,
-                            player_name_mapping["fbref_player_name"],
-                            100,
-                        ]
+                    fbref_player_dict = find_dict_in_list(
+                        fbref_player_data,
+                        "name",
+                        player_name_mapping["fbref_player_name"],
+                    )
+
+                    if not fbref_player_dict:
+                        raise ValueError(
+                            f"FBRef player not found for: {fpl_player_name}"
+                        )
+
+                    player_id_crosswalk.append(
+                        {
+                            "fpl_player_id": fpl_player_dict["fpl_row_id"],
+                            "fbref_player_id": fbref_player_dict["fbref_row_id"],
+                            "fpl_player_name": fpl_player_name,
+                            "fbref_player_name": player_name_mapping[
+                                "fbref_player_name"
+                            ],
+                        }
                     )
                 else:
-                    matches.append([fpl_player_name, match[0], match_score])
+                    fbref_player_dict = find_dict_in_list(
+                        fbref_player_data,
+                        "name",
+                        match[0],
+                    )
 
-        # Convert the output to pandas df
-        df = pd.DataFrame(
-            matches, columns=["fpl_player_name", "fbref_player_name", "score"]
-        )
+                    if not fbref_player_dict:
+                        raise ValueError(
+                            f"FBRef player not found for: {fpl_player_name}"
+                        )
 
-        df_sorted = df.sort_values(by=["score"])
-        print(df_sorted)
+                    player_id_crosswalk.append(
+                        {
+                            "fpl_player_id": fpl_player_dict["fpl_row_id"],
+                            "fbref_player_id": fbref_player_dict["fbref_row_id"],
+                            "fpl_player_name": fpl_player_name,
+                            "fbref_player_name": match[0],
+                        }
+                    )
+
+        # Create df containing player crosswalk data
+        player_id_crosswalk_df = pd.DataFrame(player_id_crosswalk)
+
+        print(player_id_crosswalk_df)
