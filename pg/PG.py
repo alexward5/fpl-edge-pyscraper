2323a636b0e427306082831d198c5697b98d9af2
Alex Ward <alexanderward5@gmail.com>
2024-10-26 15:37:55 -0600

    always drop view before creating a new one

--- pg/PG.py
+++ pg/PG.py
@@ -108,6 +108,13 @@ class PG:
     ) -> None:
         with self.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
             cur.execute(
+                sql.SQL("DROP VIEW IF EXISTS {schema}.{view_name}").format(
+                    schema=sql.Identifier(schema),
+                    view_name=sql.Identifier(view_name),
+                ),
+            )
+
+            cur.execute(
                 sql.SQL(
                     "CREATE OR REPLACE VIEW {schema}.{view_name} AS {view_query}"
                 ).format(
