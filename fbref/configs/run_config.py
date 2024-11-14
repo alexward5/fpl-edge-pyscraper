75d5dc901491737b31b672f3fced17a11a9b28c4
Alex Ward <alexanderward5@gmail.com>
2024-11-13 20:28:46 -0700

    update run config

--- fbref/configs/run_config.py
+++ fbref/configs/run_config.py
@@ -5,7 +5,10 @@ fbref_table_config: dict[str, Any] = {
     "table_url": "https://fbref.com/en/comps/9/Premier-League-Stats",
     "table_index": 0,
     "header_row_index": 0,
-    "row_id_input_fields": ["team"],
+    "row_id_config": {
+        "column_name": "fbref_team_id",
+        "row_id_input_fields": ["team"],
+    },
     "row_filters": [
         # Rows where expression is True will be filtered from table data
         {"column_name": "team", "comparison": "!=", "value": "Aston Villa"}
@@ -15,18 +18,25 @@ fbref_table_config: dict[str, Any] = {
         "table_name": "fbref_team_players_standard",
         "table_index": 0,
         "header_row_index": 1,
-        "row_id_input_fields": ["player", "team"],
+        "row_id_config": {
+            "column_name": "fbref_player_id",
+            "row_id_input_fields": ["player", "team"],
+        },
         "hyperlink_data_stat": "team",
         "include_parent_fields": "team",
-        # "child_table_config": {
-        #     "table_name": "fbref_player_matchlog",
-        #     "table_index": 0,
-        #     "header_row_index": 1,
-        #     "hyperlink_data_stat": "matches",
-        #     "include_parent_fields": "player",
-        #     "row_filters": [
-        #         {"column_name": "comp", "comparison": "!=", "value": "Premier League"}
-        #     ],
-        # },
+        "child_table_config": {
+            "table_name": "fbref_player_matchlog",
+            "table_index": 0,
+            "header_row_index": 1,
+            "row_id_config": {
+                "column_name": "fbref_player_matchlog_id",
+                "row_id_input_fields": ["player", "team", "round"],
+            },
+            "hyperlink_data_stat": "matches",
+            "include_parent_fields": ["player", "team", "fbref_player_id"],
+            "row_filters": [
+                {"column_name": "comp", "comparison": "!=", "value": "Premier League"}
+            ],
+        },
     },
 }
