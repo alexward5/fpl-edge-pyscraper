c61d6662b0dc50481298af2cafc422483da773bb
Alex Ward <alexanderward5@gmail.com>
2024-11-13 19:16:32 -0700

    check if hyperlink exists before adding it to child table urls list

--- fbref/helpers/get_child_table_urls.py
+++ fbref/helpers/get_child_table_urls.py
@@ -9,7 +9,7 @@ def get_child_table_urls(
     for row_data in fbref_table.table_rows:
         child_table_url = next(
             item for item in row_data if item["data_stat"] == hyperlink_data_stat
-        )["data_hyperlink"]
+        ).get("data_hyperlink")
 
         if child_table_url:
             child_table_urls.append(child_table_url)
