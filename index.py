ff6ee1ab55561e3389ab98c3feb1dd7d63ad3cc9
Alex Ward <alexanderward5@gmail.com>
2025-02-28 18:02:26 -0700

    conditionally run fbref team data scraper

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from fbref.configs.run_config import fbref_player_data_config
+from fbref.configs.run_config import fbref_team_data_config
 from run_scripts.create_schema_and_tables import create_schema_and_tables
 from run_scripts.create_views import create_views
 from run_scripts.generate_player_crosswalk import generate_player_crosswalk
@@ -8,7 +9,8 @@ from run_scripts.process_fpl_api_data import process_fpl_api_data
 SCHEMA_NAME = "test_schema"
 
 CREATE_SCHEMA_AND_TABLES = True
-PROCESS_FBREF_TABLES = True
+PROCESS_FBREF_PLAYER_DATA = True
+PROCESS_FBREF_TEAM_DATA = True
 PROCESS_FPL_API_DATA = True
 GENERATE_PLAYER_CROSSWALK = True
 CREATE_PLAYER_VIEWS = True
@@ -16,11 +18,18 @@ CREATE_PLAYER_VIEWS = True
 if CREATE_SCHEMA_AND_TABLES:
     create_schema_and_tables(schema_name=SCHEMA_NAME)
 
-if PROCESS_FBREF_TABLES:
+if PROCESS_FBREF_PLAYER_DATA:
     process_fbref_table(
         schema_name=SCHEMA_NAME,
         table_url=fbref_player_data_config["table_url"],
         fbref_table_config=fbref_player_data_config,
+    )
+
+if PROCESS_FBREF_TEAM_DATA:
+    process_fbref_table(
+        schema_name=SCHEMA_NAME,
+        table_url=fbref_team_data_config["table_url"],
+        fbref_table_config=fbref_team_data_config,
     )
 
 if PROCESS_FPL_API_DATA:
