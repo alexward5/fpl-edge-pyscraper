cb876cb9e0727bc7e669297ab6e767a8b2c2ab5b
Alex Ward <alexanderward5@gmail.com>
2025-07-14 17:27:53 -0600

    implement functionality to ingest fpl player gameweek data

--- fpl_api/FPL_API.py
+++ fpl_api/FPL_API.py
@@ -10,8 +10,10 @@ class FPL_API:
     def __init__(
         self,
     ) -> None:
-        self.team_code_mapping: dict = {}
+        self.team_id_mapping: dict = {}
+        self.player_id_mapping: dict = {}
         self.player_data: list = []
+        self.player_gameweek_data: list = []
 
         self._api_response_raw = {}
 
@@ -21,17 +23,19 @@ class FPL_API:
         response.raise_for_status()
 
         # Sleep to avoid reaching rate limit
-        sleep(6)
+        sleep(3)
 
         self._api_response_raw = response.json()
 
         # Check that fpl season data exists before proceeding
         if self._api_response_raw.get("events"):
-            self._get_team_code_mapping()
+            self._create_team_id_mapping()
+            self._create_player_id_mapping()
             self._parse_player_data()
+            self._parse_player_gameweek_data()
 
-    def _get_team_code_mapping(self) -> None:
-        # Map fpl team codes to fbref team names
+    def _create_team_id_mapping(self) -> None:
+        # Map fpl team ids to fbref team names
         team_name_mappings = []
         with open("fpl_api/mappings/team_names.csv", "r") as csvfile:
             dict_reader = csv.DictReader(csvfile)
@@ -40,7 +44,7 @@ class FPL_API:
         teams = self._api_response_raw["teams"]
 
         for team in teams:
-            team_code = team["code"]
+            team_id = team["id"]
             team_name = team["name"]
 
             team_name_mapping = find_dict_in_list(
@@ -53,13 +57,35 @@ class FPL_API:
             else:
                 fbref_team_name = team_name_mapping["fbref_team_name"]
 
-            self.team_code_mapping[team_code] = fbref_team_name
+            self.team_id_mapping[team_id] = fbref_team_name
+
+    # Map fpl player ids to player first name, second name and fbref team name
+    def _create_player_id_mapping(self) -> None:
+        players = self._api_response_raw["elements"]
+
+        for player in players:
+            self.player_id_mapping[player["id"]] = {
+                "first_name": player["first_name"],
+                "second_name": player["second_name"],
+                "fbref_team_name": self.team_id_mapping[player["team"]],
+            }
 
     def _parse_player_data(self) -> None:
         players = self._api_response_raw["elements"]
 
-        # Add mapped fbref team name to player data
-        for player in players:
-            player["fbref_team_name"] = self.team_code_mapping[player["team_code"]]
-
         self.player_data = players
+
+    def _parse_player_gameweek_data(self) -> None:
+        player_ids = list(self.player_id_mapping.keys())
+
+        for player_id in player_ids:
+            response = requests.get(
+                f"https://fantasy.premierleague.com/api/element-summary/{player_id}/"
+            )
+            response.raise_for_status()
+
+            # Sleep to avoid reaching rate limit
+            sleep(3)
+
+            for gw in response.json()["history"]:
+                self.player_gameweek_data.append(gw)
--- run_scripts/process_fpl_api_data.py
+++ run_scripts/process_fpl_api_data.py
@@ -4,6 +4,9 @@ from fpl_api.FPL_API import FPL_API
 from pg.configs.table_configs.fpl_player_data import (
     fpl_player_data as fpl_player_data_config,
 )
+from pg.configs.table_configs.fpl_player_gameweek_data import (
+    fpl_player_gameweek_data as fpl_player_gameweek_data_config,
+)
 from pg.PG import PG
 from utils.clean_cell_data import clean_cell_data
 from utils.fill_df_missing_values import fill_df_missing_values
@@ -13,7 +16,7 @@ from utils.set_df_dtypes import set_df_dtypes
 pg = PG(dbname="postgres", user="postgres")
 
 
-def process_fpl_api_data(schema_name: str) -> None:
+def process_fpl_player_data(schema_name: str) -> None:
     fpl_player_data_column_configs = fpl_player_data_config["table_column_configs"]
     fpl_player_data_column_names = [
         column_config["column_name"] for column_config in fpl_player_data_column_configs
@@ -23,10 +26,9 @@ def process_fpl_api_data(schema_name: str) -> None:
 
     fpl_players_df = pd.DataFrame.from_records(fpl_api_data.player_data)
 
-    # Drop columns from df that are not in table column config
-    for col in fpl_players_df.columns:
-        if col not in fpl_player_data_column_names:
-            fpl_players_df = fpl_players_df.drop(col, axis=1)
+    fpl_players_df["fbref_team_name"] = fpl_players_df["id"].map(
+        fpl_api_data.team_id_mapping
+    )
 
     fpl_players_df = set_df_dtypes(fpl_players_df)
     fpl_players_df = fill_df_missing_values(fpl_players_df)
@@ -35,6 +37,11 @@ def process_fpl_api_data(schema_name: str) -> None:
         row_id_input_fields=["first_name", "second_name", "fbref_team_name"],
         row_id_column_name="fpl_player_id",
     )
+
+    # Drop columns from df that are not in table column config
+    for col in fpl_players_df.columns:
+        if col not in fpl_player_data_column_names:
+            fpl_players_df = fpl_players_df.drop(col, axis=1)
 
     # Insert dataframe rows into postgres table
     for _, row in fpl_players_df.iterrows():
@@ -47,3 +54,70 @@ def process_fpl_api_data(schema_name: str) -> None:
             row_values=cleaned_row_values,
             update_on="fpl_player_id",
         )
+
+
+def process_fpl_player_gameweek_data(schema_name: str) -> None:
+    fpl_player_gameweek_data_column_configs = fpl_player_gameweek_data_config[
+        "table_column_configs"
+    ]
+    fpl_player_gameweek_data_column_names = [
+        column_config["column_name"]
+        for column_config in fpl_player_gameweek_data_column_configs
+    ]
+
+    fpl_api_data = FPL_API()
+
+    fpl_players_df = pd.DataFrame.from_records(fpl_api_data.player_gameweek_data)
+
+    # Add 3 fields needed to create unique player id
+    fpl_players_df["first_name"] = fpl_players_df["element"].map(
+        lambda x: fpl_api_data.player_id_mapping[x]["first_name"]
+    )
+    fpl_players_df["second_name"] = fpl_players_df["element"].map(
+        lambda x: fpl_api_data.player_id_mapping[x]["second_name"]
+    )
+    fpl_players_df["fbref_team_name"] = fpl_players_df["element"].map(
+        lambda x: fpl_api_data.player_id_mapping[x]["fbref_team_name"]
+    )
+
+    fpl_players_df = set_df_dtypes(fpl_players_df)
+    fpl_players_df = fill_df_missing_values(fpl_players_df)
+    fpl_players_df = generate_row_ids(
+        df=fpl_players_df,
+        row_id_input_fields=["first_name", "second_name", "fbref_team_name"],
+        row_id_column_name="fpl_player_id",
+    )
+
+    # Create unique row id for each player/gameweek
+    fpl_players_df = generate_row_ids(
+        df=fpl_players_df,
+        row_id_input_fields=["fpl_player_id", "fixture"],
+        row_id_column_name="fpl_player_gameweek_id",
+    )
+
+    # Create custom date field using kickoff timestamp
+    fpl_players_df["fpl_match_date"] = (
+        fpl_players_df["kickoff_time"].str.split("T").str[0]
+    )
+
+    # Drop columns from df that are not in table column config
+    for col in fpl_players_df.columns:
+        if col not in fpl_player_gameweek_data_column_names:
+            fpl_players_df = fpl_players_df.drop(col, axis=1)
+
+    # Insert dataframe rows into postgres table
+    for _, row in fpl_players_df.iterrows():
+        cleaned_row_values = [clean_cell_data(val) for val in row.to_list()]
+
+        pg.insert_row(
+            schema=schema_name,
+            table_name="fpl_player_gameweek_data",
+            column_names=fpl_players_df.columns.to_list(),
+            row_values=cleaned_row_values,
+            update_on="fpl_player_gameweek_id",
+        )
+
+
+def process_fpl_api_data(schema_name: str) -> None:
+    process_fpl_player_data(schema_name=schema_name)
+    process_fpl_player_gameweek_data(schema_name=schema_name)
